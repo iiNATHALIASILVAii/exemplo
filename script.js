e1a16da46a583fff08ebf511a84b9b040c1ad0bf
iiNATHALIASILVAii <gabriellynathalia@outlook.com>
2025-08-12 10:54:53 -0300

    ajuste

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+  Elabore um programa para um cinema, onde o usuario possa digitar o titulo e a duração de um filme em minutos.
+  Exiba o titulo do silme e converta a duração para horas e minutos.
+
+*/
+
